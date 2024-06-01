a306b869bc4dad9b19b427a963b5b767de112782
Arpit Sharma <AS.73881@GMAIL.COM>
2024-06-01 06:35:46 +0530

    Float

---
+++ Practice-Set-01/3 - Float.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  float abc = 20.2;
+  printf("The size of float is %ld byte\n",sizeof(float));
+  printf("The value of float is %f\n",abc);
+  return 0;
+}
