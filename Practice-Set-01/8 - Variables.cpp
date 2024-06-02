7546bcd57afd650dbc70b7e2b0c02c4f8a66cb82
Arpit Sharma <AS.73881@GMAIL.COM>
2024-06-03 05:09:51 +0530

    Variables

---
+++ Practice-Set-01/8 - Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  int var;
+
+  //decimal
+  var = 22;
+
+  //binary
+  // var = 0b00010110;
+
+  //hexadecimal
+  // var = 0x16;
+
+  std::cout<<"The value of var is "<<var<<std::endl;
+
+  return 0;
+}
