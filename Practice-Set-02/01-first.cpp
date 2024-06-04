22bc92b0f9b39e802ed042207e69201763514493
Arpit Sharma <AS.73881@GMAIL.COM>
2024-06-04 05:53:37 +0530

    First

---
+++ Practice-Set-02/01-first.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  // std::cout << "hello";
+
+  // cout << "hello coder army\n";
+  // cout << "hello coder army";
+
+  cout << "hello coder army" << endl
+       << "hello coder army" << endl;
+
+  cout << 3 + 2 << endl;
+  cout << "3+2" << endl;
+
+  // variable declaration
+
+  // integer
+  int a;
+  a = 10;
+  int b = 20;
+  cout << a << endl;
+  cout << "a" << endl;
+
+  int c = 5, d = 8;
+  cout << c + d << endl;
+
+  // character
+  char e = 'a';
+  cout << e << endl;
+
+  // float
+  float f = 2.65;
+
+  // double
+  double g = 3.56455;
+
+  // boolean
+  // bool h = 0;
+  bool h = true;
+  cout << h << endl;
+
+  return 0;
+}
