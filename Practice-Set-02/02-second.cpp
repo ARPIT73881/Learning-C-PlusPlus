6abd52775148140d04812b432393d6b71ec3a9c1
Arpit Sharma <AS.73881@GMAIL.COM>
2024-06-04 05:53:59 +0530

    Second

---
+++ Practice-Set-02/02-second.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  // int a, b;
+  // cin >> a >> b;
+  // cout << a + b;
+
+  // cout << "Enter the first number : ";
+  // cin >> a;
+  // cout << "Enter the second number : ";
+  // cin >> b;
+  // cout << "The sum is : " << a + b << endl;
+
+  // int a = 10;
+  // char c = 'b';
+  // a = c;
+  // cout << a; //output is 98
+
+  int a = 66;
+  char c = 'b';
+  c = a;
+  cout << c; // output is B
+
+  return 0;
+}
